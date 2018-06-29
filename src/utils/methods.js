be18f352811d4b1de47449e52f38dd6fb8a0ab09
Gary <garyh_sir@163.com>
2018-06-29 09:42:14 +0800

    WeChat_ShopApp 首页，分类页，我的页面完成，及相关业务逻辑的完成

---
+++ src/utils/methods.js
@@ -0,0 +1,23 @@
+// API地址
+var APIHost = 'https://www.zhengzhicheng.cn/'
+
+// 所有页面公用的方法
+var methods = {
+  /**发送request请求
+   * url:api地址(单个功能模块数据API地址)
+   */
+  request: options => {
+    console.log(`${APIHost}${options.url}`)
+    wx.request({
+      url: `${APIHost}${options.url}`,
+      method: options.method || 'GET',
+      data: options.data || {},
+      header: options.header || 'application/x-www-form-urlencoded',
+      success: options.success || (backData => console.log(backData)),
+      fail: err => console.log(PositionError)
+    })
+  }
+}
+
+// 暴露出methods
+module.exports.methods = methods
